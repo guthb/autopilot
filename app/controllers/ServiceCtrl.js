4191032f5fe3748ae6a9b4c8d86383e34aaf58cf
Bradley Guthrie <guppy96@gmail.com>
2016-06-30 22:51:08 -0500

    updated styling

--- app/controllers/ServiceCtrl.js
+++ app/controllers/ServiceCtrl.js
@@ -19,7 +19,7 @@ app.controller("ServiceCtrl", function($scope, $location, $rootScope, FirebaseFa
 
   var currentTime = new Date();
     $scope.currentTime = currentTime;
-    $scope.month = ['Januar', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'];
+    $scope.month = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'];
     $scope.monthShort = ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec'];
     $scope.weekdaysFull = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
     $scope.weekdaysLetter = ['S', 'M', 'T', 'W', 'T', 'F', 'S'];
