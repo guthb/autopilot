7054aba5e7dc5ea0d561c2df60dbd607bd825802
Bradley Guthrie <guppy96@gmail.com>
2017-01-12 19:07:14 -0600

    restructured for hosting

---
+++ public/app/appConstants.js
@@ -0,0 +1,10 @@
+app.constant ('firebaseURL', 'https://ng-bg-pm.firebaseio.com/');
+app.constant ('recallURL', 'http://www.nhtsa.gov/webapi/api/Recalls/vehicle/');
+
+app.constant("FIREBASE_CONFIG", {
+  apiKey: "AIzaSyDH2ADqPbVg8gRta3gzr6aWzJ6Jttl1DMs",
+  authDomain: "ng-bg-pm.firebaseapp.com",
+  databaseURL: "https://ng-bg-pm.firebaseio.com",
+  storageBucket: "ng-bg-pm.appspot.com",
+  messagingSenderId: "189585738347"
+});
